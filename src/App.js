6aaba5237b06a40ce6bead237bc5fb2557a77f9a
Sodara Sou <sodarasou168@gmail.com>
2023-05-12 18:04:56 +0700

    Change size of the main container

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Header />
-      <div className="max-w-7xl mx-auto">
+      <div className="container mx-auto">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Project />} />
