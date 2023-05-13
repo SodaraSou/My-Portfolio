97fa6c1e97e3dd4e5ecf7f23b16e4638906db726
Sodara Sou <sodarasou168@gmail.com>
2023-05-13 21:10:27 +0700

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Project from "./pages/Project";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <Header />
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-7xl mx-auto flex-1">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Project />} />
@@ -18,6 +18,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
+      <Footer />
     </Router>
   );
 }
