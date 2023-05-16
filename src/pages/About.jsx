54013797dc9b49148e7216416cf59af0fbf86e1d
Sodara Sou <sodarasou168@gmail.com>
2023-05-16 15:04:38 +0700

    Beginning

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,26 @@
+import myImage from "../images/SodaraSou.jpg";
+
 function About() {
   return (
-    <div>About</div>
-  )
+    <section id="about">
+      <div className="p-10 text-center md:text-left">
+        <h1 className="text-5xl font-bold md:text-left mb-10">About Me</h1>
+        <div className="flex justify-center items-center flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5">
+          <div>
+            <p className="text-xl md:text-2xl">
+              My name is Sodara Sou and I am a sophomore in the ITE department
+              at the Royal University of Phnom Penh. I find programming to be a
+              fascinating field and enjoy the challenge of creating software
+              from scratch. I have a particular affinity for web development and
+              enjoy building responsive and visually appealing websites. I find
+              the process of designing and coding a website to be both creative
+              and technically challenging.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,7 +1,60 @@
+import ProjectThreeJS from "../images/ThreeJS.png";
+import TodayILearned from "../images/TodayILearned.png";
+import CubeGamingStore from "../images/CubeGamingStore.png";
+
 function Project() {
   return (
     <section id="project">
-      <h1>Hello</h1>
+      <div className="p-10">
+        <h1 className="text-5xl font-bold text-center md:text-left mb-10">
+          Project
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-10 h-full">
+          <div className="bg-daraBlack rounded-2xl hover:scale-105 duration-500">
+            <a href="#">
+              <img
+                src={ProjectThreeJS}
+                alt="ProjectThreeJS"
+                className="rounded-t-2xl w-full h-[180px]"
+              />
+              <div className="p-5 space-y-3">
+                <h5 className="text-2xl font-bold">Three.js Project</h5>
+                <div className="flex items-center text-sm space-x-3 font-medium">
+                  <span className="bg-orange-600 px-2 rounded-md">HTML</span>
+                  <span className="bg-blue-700 px-2 rounded-md">CSS</span>
+                  <span className="bg-white text-black px-2 rounded-md">
+                    Three.js
+                  </span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div className="bg-daraBlack rounded-2xl hover:scale-105 duration-500">
+            <a href="#">
+              <img
+                src={TodayILearned}
+                alt="ProjectThreeJS"
+                className="rounded-t-2xl w-full h-[180px]"
+              />
+              <div className="p-5">
+                <h5 className="text-2xl font-bold">Today I Learned</h5>
+              </div>
+            </a>
+          </div>
+          <div className="bg-daraBlack rounded-2xl hover:scale-105 duration-500">
+            <a href="#">
+              <img
+                src={CubeGamingStore}
+                alt="ProjectThreeJS"
+                className="rounded-t-2xl w-full h-[180px]"
+              />
+              <div className="p-5">
+                <h5 className="text-2xl font-bold">Cube Gaming Store </h5>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
