1a66cfca7f11855d564710fdcb41622ba0aa3560
Sodara Sou <sodarasou168@gmail.com>
2023-08-27 09:02:09 +0700

    Fixed grid

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,13 +4,8 @@ function Contact() {
   return (
     <section id="contact" className="p-10 py-20 md:p-20 text-black">
       <div className="max-w-5xl mx-auto">
-        <h1 className="text-3xl mb-10 text-center">Contact</h1>
-        <div className="flex flex-col md:flex-row justify-center items-center md:space-x-10">
-          {/* <div className="p-20 shadow-xl rounded-xl flex flex-col items-center w-full md:w-auto">
-            <img src={phone} alt="phone" className="w-10 md:w-11 mb-3" />
-            <h1 className="mb-5">Phone</h1>
-            <p>012-354-987</p>
-          </div> */}
+        <h1 className="text-3xl mb-10 text-center font-bold">Contact</h1>
+        {/* <div className="flex flex-col md:flex-row justify-center items-center md:space-x-10">
           <div className="p-20 shadow-xl rounded-xl flex flex-col items-center w-full md:w-1/2">
             <img src={mail} alt="mail" className="w-10 md:w-11 mb-3" />
             <h1 className="mb-5">Email</h1>
@@ -24,6 +19,18 @@ function Contact() {
             />
             <h1 className="mb-5">Location</h1>
             <p>Phnom Penh</p>
+          </div>
+        </div> */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-[600px] md:h-[300px]">
+          <div className="rounded-3xl shadow-xl flex flex-col justify-center items-center gap-4">
+            <img src={mail} alt="mail" className="w-10" />
+            <h1>Email</h1>
+            <p className="font-bold">sodarasou168@gmail.com</p>
+          </div>
+          <div className="rounded-3xl shadow-xl flex flex-col justify-center items-center gap-4">
+            <img src={location} alt="location" className="w-8" />
+            <h1>Location</h1>
+            <p className="font-bold">Phnom Penh</p>
           </div>
         </div>
       </div>
